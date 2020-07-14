e8f9487008641fec56a69ce5a12853782c71a0e8
estherkimyunjung <estherkimyunjung@gmail.com>
2020-07-14 14:02:16 -0500

    add detail

--- Gemfile
+++ Gemfile
@@ -39,3 +39,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'http'
 gem 'nokogiri'
 # gem 'httparty'
+gem 'zillow4r'
--- app/web_crolling.rb
+++ app/web_crolling.rb
@@ -24,7 +24,33 @@ def scarper
   url = 'https://www.zillow.com/houston-tx/'
   unparsed_page = HTTP.get(url).to_s
   parsed_page = Nokogiri::HTML(unparsed_page)
+  houses = Array.new
+  # house_listings = parsed_page.css('article.list-card') #40 houses
+  house_listings = parsed_page.css('ul.photo-cards>li') #40 houses
+  page = 1
+  per_page = house_listings.count #40
+  total = parsed_page.css('.result-count').text.split(' ')[0].gsub(',','').to_i
+  last_page = (total.to_f / per_page.to_f).round
+  house_listings.each do |house_listing|
+    # lat =JSON[house_listing.at('script[type="application/ld+json"]').text]['geo']['latitude']
+    house = {
+      address: house_listing.css('article.list-card address').text,
+      zipcode: house_listing.css('article.list-card address').text.split(' ').last,
+      # latitude: lat,
+      # longitude: JSON[house_listing.at('script[type="application/ld+json"]').text]['geo']['longitude'],
+      prices: house_listing.css('article.list-card div.list-card-price').text,
+      detail:house_listing.css('article.list-card ul.list-card-details').text,
+      # bds:house_listing.css('ul.list-card-details li')[0].text,
+      # bath:house_listing.css('ul.list-card-details li')[1].text,
+      # sqft:house_listing.css('ul.list-card-details li')[2].text,
+      url: house_listing.css('article.list-card a.list-card-img').map{|link| link.attributes["href"].text},
+      image: house_listing.css('article.list-card img').map{|i| i.attributes["src"].value}
+    }
+    houses << house
+    # end
+  end
   byebug
+
   # house: parsed_page.css('article.list-card')
   # address: home.css('address')
   # prices: home.css('div.list-card-price')
